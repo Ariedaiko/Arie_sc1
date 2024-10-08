a77b0d21357bba699d97203c9783dd3c4f0d2706
Arie_dayo <157713538+Ariedaiko@users.noreply.github.com>
2024-10-08 16:08:09 +0900

    Update F.lua

--- F.lua
+++ F.lua
@@ -1,2 +1,3 @@
-gg.alert('ありえの旧スクリプトですよ?\nなんで販売用にしてんすかplknさんよぉ')
-print('新スクリプトはこちらから\n95式のDiscordサーバー︰https://discord.gg/syBM49Mw4x\nありえのDiscordサーバー︰https://discord.gg/5MjNPRAmJT')
+gg.alert('\"ありえ\"の旧スクリプトです。\nなんで販売用にしてんすかplknさんよぉ')
+print('新スクリプトはこちらから\n95式のDiscordサーバー︰https://discord.gg/syBM49Mw4x\nありえのDiscordサーバー︰https://discord.gg/5MjNPRAmJT\nplknの詐欺にあった人はありえのサーバーから\nチケットを発行するようお願い致します。')
+os.exit(gg.setVisible(true))
