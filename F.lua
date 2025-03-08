606fc78338a8c64489df231928a066e6e17c5c39
Arie_dayo <157713538+Ariedaiko@users.noreply.github.com>
2025-03-08 21:31:01 +0900

    Delete F.lua

--- F.lua
+++
@@ -1,3 +0,0 @@
-gg.alert('\"ありえ\"の旧スクリプトです。\nなんで販売用にしてんすかplknさんよぉ')
-print('新スクリプトはこちらから\nありえのDiscordサーバー︰https://discord.gg/5MjNPRAmJT\nplknの詐欺にあった人はありえのDiscordサーバーから\nチケットを発行するようお願い致します。')
-os.exit(gg.setVisible(true))
